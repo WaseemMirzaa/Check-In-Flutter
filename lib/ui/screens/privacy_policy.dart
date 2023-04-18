949ffe977d17d9980dcebd978457b06fbdc4b9c2
UsmanMirza1122 <usmanmirza.developlogix@gmail.com>
2023-04-18 15:33:09 +0500

    privacy policy, terms conditions

--- lib/ui/screens/privacy_policy.dart
+++ lib/ui/screens/privacy_policy.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:checkinmod/ui/screens/persistent_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import 'package:sizer/sizer.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import '../../utils/colors.dart';
 import '../../utils/styles.dart';
@@ -14,6 +17,8 @@ class PrivacyPolicy extends StatefulWidget {
 }
 
 class _PrivacyPolicyState extends State<PrivacyPolicy> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +47,7 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
       ),
       body: Container(
         margin: EdgeInsets.only(top: 10),
-        padding: EdgeInsets.only(top: 30, left: 30, right: 30),
+        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
@@ -58,12 +63,56 @@ class _PrivacyPolicyState extends State<PrivacyPolicy> {
             ),
           ],
         ),
-        child: poppinsText(
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.Enim diam vulputate ut pharetra sit amet. In hac habitasse platea dictumst quisque sagittis purus. Vel risus commodo viverra maecenas accumsan lacus vel. Arcu non sodales neque sodales ut etiam. Lectus arcu bibendum at varius vel pharetra vel turpis nunc. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Risus quis varius quam quisque id diam. Blandit aliquam etiam erat velit scelerisque in. Sed felis eget velit aliquet sagittis id consectetur purus. Est placerat in egestas erat imperdiet sed. Elit pellentesque habitant morbi tristique senectus et netus. Porttitor leo a diam sollicitudin tempor id eu. Turpis massa tincidunt dui ut ornare lectus sit ",
-            14,
-            regular,
-            textColor),
+        child: Builder(builder: (BuildContext context) {
+          return WebView(
+            initialUrl:
+                "https://www.freeprivacypolicy.com/live/fc6c8c08-7126-407d-b16b-5ac249c71a80",
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url.startsWith('https://www.youtube.com/')) {
+                print('blocking navigation to $request}');
+                return NavigationDecision.prevent;
+              }
+              print('allowing navigation to $request');
+              return NavigationDecision.navigate;
+            },
+            onPageStarted: (String url) {
+              print('Page started loading: $url');
+            },
+            onPageFinished: (String url) {
+              print('Page finished loading: $url');
+            },
+            gestureNavigationEnabled: true,
+          );
+        }),
       ),
+      // Container(
+      //   margin: EdgeInsets.only(top: 10),
+      //   padding: EdgeInsets.only(top: 30, left: 30, right: 30),
+      //   decoration: BoxDecoration(
+      //     color: Colors.white,
+      //     borderRadius: BorderRadius.only(
+      //       topRight: Radius.circular(30),
+      //       topLeft: Radius.circular(30),
+      //     ),
+      //     boxShadow: [
+      //       BoxShadow(
+      //         color: Colors.grey.withOpacity(0.2),
+      //         blurRadius: 5,
+      //         //   spreadRadius: -12,
+      //         offset: Offset(0, -3), // changes position of shadow
+      //       ),
+      //     ],
+      //   ),
+      //   child: poppinsText(
+      //       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.Enim diam vulputate ut pharetra sit amet. In hac habitasse platea dictumst quisque sagittis purus. Vel risus commodo viverra maecenas accumsan lacus vel. Arcu non sodales neque sodales ut etiam. Lectus arcu bibendum at varius vel pharetra vel turpis nunc. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Risus quis varius quam quisque id diam. Blandit aliquam etiam erat velit scelerisque in. Sed felis eget velit aliquet sagittis id consectetur purus. Est placerat in egestas erat imperdiet sed. Elit pellentesque habitant morbi tristique senectus et netus. Porttitor leo a diam sollicitudin tempor id eu. Turpis massa tincidunt dui ut ornare lectus sit ",
+      //       14,
+      //       regular,
+      //       textColor),
+      // ),
     );
   }
 }
--- lib/ui/screens/terms_conditions.dart
+++ lib/ui/screens/terms_conditions.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:checkinmod/ui/screens/persistent_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import 'package:sizer/sizer.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import '../../utils/colors.dart';
 import '../../utils/styles.dart';
@@ -14,6 +17,8 @@ class TermsAndConditions extends StatefulWidget {
 }
 
 class _TermsAndConditionsState extends State<TermsAndConditions> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +48,7 @@ class _TermsAndConditionsState extends State<TermsAndConditions> {
       ),
       body: Container(
         margin: EdgeInsets.only(top: 10),
-        padding: EdgeInsets.only(top: 30, left: 30, right: 30),
+        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
@@ -59,11 +64,31 @@ class _TermsAndConditionsState extends State<TermsAndConditions> {
             ),
           ],
         ),
-        child: poppinsText(
-            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.Enim diam vulputate ut pharetra sit amet. In hac habitasse platea dictumst quisque sagittis purus. Vel risus commodo viverra maecenas accumsan lacus vel. Arcu non sodales neque sodales ut etiam. Lectus arcu bibendum at varius vel pharetra vel turpis nunc. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Risus quis varius quam quisque id diam. Blandit aliquam etiam erat velit scelerisque in. Sed felis eget velit aliquet sagittis id consectetur purus. Est placerat in egestas erat imperdiet sed. Elit pellentesque habitant morbi tristique senectus et netus. Porttitor leo a diam sollicitudin tempor id eu. Turpis massa tincidunt dui ut ornare lectus sit ",
-            14,
-            regular,
-            textColor),
+        child: Builder(builder: (BuildContext context) {
+          return WebView(
+            initialUrl:
+                "https://www.freeprivacypolicy.com/live/0ccc6661-8efc-4732-b790-0663f523ed4a",
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url.startsWith('https://www.youtube.com/')) {
+                print('blocking navigation to $request}');
+                return NavigationDecision.prevent;
+              }
+              print('allowing navigation to $request');
+              return NavigationDecision.navigate;
+            },
+            onPageStarted: (String url) {
+              print('Page started loading: $url');
+            },
+            onPageFinished: (String url) {
+              print('Page finished loading: $url');
+            },
+            gestureNavigationEnabled: true,
+          );
+        }),
       ),
     );
   }
